b222780ea5b4f0ff9fac1d7bc365477f436899e3
manikantha92 <manikantha92@gmail.com>
2018-07-17 16:04:38 -0400

    updated the main function

--- src/main/webapp/templates/display/findAllShowtime.controller.js
+++ src/main/webapp/templates/display/findAllShowtime.controller.js
@@ -5,7 +5,7 @@
 
     function FindAllShowtimeController($scope, $location, $http) {
         function init() {
-            $http.get('https://api.internationalshowtimes.com/v4/movies/?countries=US&apikey=').
+            $http.get('https://api.internationalshowtimes.com/v4/movies/?countries=US&apikey=VSMQVz6XK4lAeSyZJ0RM6ezSH8UWpbmp').
             then(function(response) {
                 console.log(response.data);
                 $scope.allShowtimes = response.data;
