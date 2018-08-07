ee89f9baf48621a61ea9cb662ad569bb748e1402
manikantha92 <manikantha92@gmail.com>
2018-08-07 00:40:35 -0400

    the foreach changes in findallshowtimes

--- src/main/webapp/templates/showtimes/findAllShowtimes.controller.js
+++ src/main/webapp/templates/showtimes/findAllShowtimes.controller.js
@@ -11,6 +11,12 @@
     	var cinemaUrl;
     	var cinemaId;
     	var id;
+    	var movieId;
+    	$scope.finalArray = [];
+    	var allMovies;
+    	var allShowtimes;
+    	var allCinemas;
+    	this.movieDetails = movieDetails;
     	
         function init() {
         	
@@ -20,24 +26,44 @@
         	
            
         	cinemaId = $routeParams.cinemaId.substring(1,$routeParams.cinemaId.length);
-        	var url4 = 'https://api.internationalshowtimes.com/v4/movies/?cinema_id='+ cinemaId +'&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5';
+        	var url4 = 'https://api.internationalshowtimes.com/v4/showtimes/?append=movies,cinemas&cinema_id='+ cinemaId +'&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5';
         	console.log("inshowtimes controller");
         	console.log(cinemaId);
         	
         	
         	$http.get(url4).
         	then(function(response) {
-                console.log(response.data);
-                $scope.allShowtimes = response.data;
+               allMovies = response.data.movies;
+               allShowtimes = response.data.showtimes;
+               allCinemas = response.data.cinemas;
+               angular.forEach(allMovies, function(movie) {
+            	    angular.forEach(allShowtimes, function(showtime) {
+            	      if(movie.id ==showtime.movie_id) {
+            	          var test = {
+            	            movieId : showtime.movie_id,
+            	            movieName: movie.slug,
+            	            start_at: showtime.start_at
+            	          }
+            	          finalArray.push(test);
+            	          
+            	      }
+            	  });
+            	    
+            	});
+               $scope.allShowtimes = finalArray[0];
+               console.log("HI "+$scope.allShowtimes);
             });
             
-        	
-           $http.get('https://api.internationalshowtimes.com/v4/showtimes/?cinema_id=30668&movie_id=39081&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5').
-           then(function(response) {
-                console.log(response.data);
-              $scope.allMovieShowtimes = response.data;
-          });
         };
         init();
+        function movieDetails(mId){
+        	movieId = mId;
+        	$http.get('https://api.internationalshowtimes.com/v4/showtimes/?cinema_id=' +cinemaId+ '&movie_id=' +movieId+ '&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5').
+            then(function(response) {
+            	console.log(response.data);
+            	console.log("inside movie details");
+                $scope.allMovieShowtimes = response.data;
+            });
+        }
     }
 })();
