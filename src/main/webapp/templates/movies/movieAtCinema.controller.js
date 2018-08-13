4bf74946b14b416ff539e91fa8e812c45163248e
manikantha92 <manikantha92@gmail.com>
2018-08-13 01:34:44 -0400

    nav bar chnages

--- src/main/webapp/templates/movies/movieAtCinema.controller.js
+++ src/main/webapp/templates/movies/movieAtCinema.controller.js
@@ -15,7 +15,7 @@
     		cinemaId = $routeParams.cinemaId.substring(1,$routeParams.cinemaId.length);
     		console.log("-------cinemaId-----------------");
     		console.log(cinemaId);
-    		$http.get('https://api.internationalshowtimes.com/v4/movies/?cinema_id='+cinemaId+'&limit=10&fields=synopsis,trailers,id,title,poster_image_thumbnail&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5').
+    		$http.get('https://api.internationalshowtimes.com/v4/movies/?cinema_id='+cinemaId+'&limit=10&fields=ratings,trailers,id,title,poster_image_thumbnail&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5').
        		then(function(response) {
        		   console.log("------------------------");
                console.log(response.data);
