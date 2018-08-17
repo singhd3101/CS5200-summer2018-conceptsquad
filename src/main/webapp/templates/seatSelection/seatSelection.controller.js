082b2c07172ee07aa555060987524860c4659262
manikantha92 <manikantha92@gmail.com>
2018-08-17 15:25:29 -0400

    seat selection UI/js part 4

--- src/main/webapp/templates/seatSelection/seatSelection.controller.js
+++ src/main/webapp/templates/seatSelection/seatSelection.controller.js
@@ -4,25 +4,29 @@
         .controller('SeatSelectionController',SeatSelectionController);
 
     function SeatSelectionController($scope, $location, $http, $routeParams, $filter) { 
-    
+    	var showtimeId;
+    	var cinemaId;
+    	var movieId;
     	function init() {
     		
+    		console.log("seat selection controller");
     		$scope.showtimeId = $routeParams.showtimeId.substring(1,$routeParams.showtimeId.length);
+    		showtimeId = $scope.showtimeId;
     		$scope.cinemaId = $routeParams.cinemaId.substring(1,$routeParams.cinemaId.length);
+    		cinemaId = $scope.cinemaId;
     		$scope.movieId = $routeParams.movieId.substring(1,$routeParams.movieId.length);
-        	console.log("in seat selection controller");
-        	console.log('https://api.internationalshowtimes.com/v4/showtimes/?append=movies,cinemas&cinema_id='+ cinemaId +'&movie_id='+ movieId +'&time_to='+ dateTo +'&movie_fields=ratings,trailers,id,title,poster_image&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5');
-        	$http.get('https://api.internationalshowtimes.com/v4/showtimes/?append=movies,cinemas&cinema_id='+ cinemaId +'&movie_id='+ movieId +'&time_to='+ dateTo +'&movie_fields=ratings,trailers,id,title,poster_image&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5').
-        	then(function(response) {
-               console.log(response.data);   
-               $scope.showtimes=response.data.showtimes;
-               $scope.movies = response.data.movies;
-               $scope.cinemas = response.data.cinemas;
-
-            	});
+        	movieId = $scope.movieId;
+        	
              
             
         };
         init();
+        
+        function checkout(seats, totalPrice){
+        	console.log("check out function");
+        	console.log("seats are "+seats);
+        	console.log("total price is"+totalPrice);
+        	//$location.url('/findAllShowtimes/:'+cinemaId);
+        }
     }
 })();
