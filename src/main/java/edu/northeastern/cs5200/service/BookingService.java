b5b3371b6ad8b14844faebe96d530c138fd4d936
Divya Singh <singh.d@husky.neu.edu>
2018-08-16 22:15:29 -0400

    cancel booking by customer

--- src/main/java/edu/northeastern/cs5200/service/BookingService.java
+++ src/main/java/edu/northeastern/cs5200/service/BookingService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -83,6 +84,16 @@ public class BookingService {
 	@PostMapping("/api/eventbooking")
 	public EventBooking createEventBooking(@RequestBody EventBooking booking) {
 		return eventBookingRepository.save(booking);
+	}
+	
+	@PutMapping("/api/booking/{bookingId}")
+	public void cancelBooking(@PathVariable("bookingId") int id) {
+		Optional<Booking> optional = bookingRepository.findById(id);
+		if(optional.isPresent()) {
+			Booking booking = optional.get();
+			booking.setStatus("Cancelled");
+			bookingRepository.save(booking);
+		}
 	}
 
 }
