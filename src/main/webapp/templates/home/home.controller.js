e70bf5d6f2ceed416cdc7a8cd5ca0bcfdd93abcd
Divya Singh <singh.d@husky.neu.edu>
2018-08-17 14:17:22 -0400

    admin crud

--- src/main/webapp/templates/home/home.controller.js
+++ src/main/webapp/templates/home/home.controller.js
@@ -13,24 +13,24 @@
         function init() {
         	
         	var pogo = $rootScope.loggedIn;
-        	console.log("pogo " + $rootScope.user.userName);
+        	//console.log("pogo " + $rootScope.user.userName);
         	$scope.pogo = $rootScope.loggedIn;
         	
-        	console.log("in home");
+        	//console.log("in home");
         	if (navigator.geolocation) {
         	    navigator.geolocation.getCurrentPosition(function(position){
         	      $scope.$apply(function(){
         	       pos = position;
         	       lat = position.coords.latitude;
         	       long = position.coords.longitude;
-        	       console.log(position.coords.latitude);
-        	       console.log(position.coords.longitude);
+        	  //     console.log(position.coords.latitude);
+        	  //     console.log(position.coords.longitude);
         	       loct='location='+lat+','+long;
-        	       console.log("chal pada");
-        	       console.log("location is "+loct);
+        	  //     console.log("chal pada");
+        	  //     console.log("location is "+loct);
                		$http.get('https://api.internationalshowtimes.com/v4/movies/?'+loct+'&limit=10&fields=ratings,synopsis,cast,trailers,id,title,poster_image&apikey=cMQZTx5UYw7m3Ddio8jS0NcFwgmEQkE5').
                		then(function(response) {
-                       console.log(response.data);
+              //         console.log(response.data);
                        $scope.allMovies = response.data;
                    });
                		
